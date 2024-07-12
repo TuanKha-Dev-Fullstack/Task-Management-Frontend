be406179f50132feeb67b82a53c4d2217c3da8d1
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-12 18:58:43 +0700

    fix: calling an API multiple times

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -7,19 +7,20 @@ import logo from '../Images/Logo_TM.png';
 import useApi from '../Hooks/UseApi';
 
 const SideBar = () => {
+    // Define state
     const url = 'http://localhost:5000/api/v1/categories';
-    const [activeLink, setActiveLink] = useState('');
+    const [activeLink, setActiveLink] = useState('tasks');
+    const { data: categories } = useApi(url);
+    // Handle link click to set active link
     const handleLinkClick = (link) => {
         setActiveLink(link);
-    }
-    const { data: categories } = useApi(url);
-    console.log(categories);
+    };
     return (
         <div className="w-1/4 p-4 my-4 bg-zinc-800 rounded-e-3xl shadow-[10px_0px_20px_rgba(0,0,0,0.5)] flex flex-col">
             <img src={logo} alt="Logo" className="w-48 h-24 self-center mb-4" />
             <ul>
                 <li className={`hover:bg-red-300 text-pink-500 rounded ${activeLink === 'tasks' ? 'bg-red-300' : ''}`}>
-                    <Link to={"/"}
+                    <Link to="/"
                         className="font-semibold text-white w-full h-full flex p-2 pl-0"
                         onClick={() => handleLinkClick('tasks')}>
                         <span className={`w-1 mr-4 rounded ${activeLink === 'tasks' ? 'bg-pink-500' : ''}`}></span>
@@ -54,7 +55,7 @@ const SideBar = () => {
                 <ul>
                     {categories?.map((category) => (
                         <li key={category.id} className={`hover:bg-blue-300 rounded ${activeLink === category.id ? 'bg-blue-300' : ''}`}>
-                            <Link to={'/category/' + category.id}
+                            <Link to={`/category/${category.id}`}
                                 className="font-semibold text-white w-full h-full flex p-2 pl-0"
                                 onClick={() => handleLinkClick(category.id)}>
                                 <span className={`w-1 mr-4 rounded ${activeLink === category.id ? 'bg-pink-500' : ''}`}></span>
@@ -72,6 +73,6 @@ const SideBar = () => {
             </button>
         </div>
     );
-}
+};
 
 export default SideBar;
--- src/Hooks/UseApi.js
+++ src/Hooks/UseApi.js
@@ -1,32 +1,32 @@
-// src/hooks/useApi.js
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useApi = (url, method = 'GET', body = null, options = {}) => {
+const useApi = (url, method = 'GET', body = null) => {
+    // define state
+    const [loading, setLoading] = useState(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
-
-    const fetchData = useCallback(async () => {
-        setError(null);
-        try {
-            const config = {
-                url,
-                method,
-                data: body,
-                ...options,
-            };
-            const response = await axios(config);
-            setData(response.data);
-        } catch (err) {
-            setError(err);
-        }
-    }, []);
-
+    // call api
     useEffect(() => {
-        fetchData();
-    }, [fetchData]);
-
-    return { data, error };
+        setLoading(true);
+        const callApi = async () => {
+            try {
+                const response = await axios({
+                    method: method,
+                    url: url,
+                    data: body
+                });
+                const res = await response?.data;
+                setData(res);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        callApi();
+    }, [url, method, body]);
+    return { data, error, loading };
 };
 
 export default useApi;
