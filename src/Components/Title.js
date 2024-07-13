c9360a7c10eee563645a221729a776d5087feff4
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-13 09:25:47 +0700

    feat: add title UI

---
+++ src/Components/Title.js
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+
+const Title = ({text, icon, color}) => {
+    return (
+        <div className={`font-semibold text-3xl ${color}`}>
+            <FontAwesomeIcon icon={icon} className="mr-2" />
+            {text}
+        </div>
+    );
+};
+
+Title.propTypes = {
+    text: PropTypes.string.isRequired,
+    icon: PropTypes.object.isRequired,
+    color: PropTypes.string.isRequired
+};
+
+export default Title;   
--- src/Pages/Tasks.js
+++ src/Pages/Tasks.js
@@ -1,6 +1,10 @@
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import Title from "../Components/Title"
+
 const Tasks = () => {
     return (
-        <div className="w-3/4 bg-zinc-800 m-4 rounded-xl">
+        <div className="w-3/4 bg-zinc-800 m-4 rounded-xl p-10">
+            <Title text="Tasks" icon={faHouse} color="text-red-500" />
         </div>
     )
 }
