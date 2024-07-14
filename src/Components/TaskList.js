4959f05867d5b2d742bd94a262701d26a8d2236e
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-14 09:46:03 +0700

    feat: handle fetch list tasks completed

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -6,19 +6,8 @@ import '../Styles/CustomScroll.css';
 import useApi from "../Hooks/UseApi";
 
 const TaskList = () => {
-    const [tasks] = useState([
-        { id: 4, name: 'Task 4', isCompleted: true },
-        { id: 2, name: 'Task 2', isCompleted: true },
-        { id: 6, name: 'Task 6', isCompleted: true },
-        { id: 8, name: 'Task 8', isCompleted: true },
-        { id: 10, name: 'Task 10', isCompleted: true },
-        { id: 12, name: 'Task 12', isCompleted: true },
-        { id: 14, name: 'Task 14', isCompleted: true },
-        { id: 16, name: 'Task 16', isCompleted: true },
-        { id: 18, name: 'Task 18', isCompleted: true },
-        { id: 20, name: 'Task 20', isCompleted: true },
-    ]);
     const {data: tasksUncompleted} = useApi("http://localhost:5000/api/v1/tasks/unfinished");
+    const {data: tasksCompleted} = useApi("http://localhost:5000/api/v1/tasks/finished");
     const [showCompleted, setShowCompleted] = useState(true);
     const handleButtonCompleted = () => {
         setShowCompleted(!showCompleted);
@@ -39,7 +28,7 @@ const TaskList = () => {
                     <span className="ml-2">Completed</span>
                 </button>
                 {showCompleted && (<div className="mt-2">
-                    {tasks.map((task) => (
+                    {tasksCompleted?.map((task) => (
                         <Task key={task.id}
                             task={task} />
                     ))}
