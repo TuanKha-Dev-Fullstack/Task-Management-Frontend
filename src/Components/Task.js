3bbb8c4d3fafad250edb83d25c60f2299c4512d2
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-18 10:22:14 +0700

    fix: custom color for input checked

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,33 +3,32 @@ import { faStar as solidStar, faEdit, faTrashAlt } from '@fortawesome/free-solid
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
+const tailwindColors = {
+    'pink-300': '#f472b6',
+    'red-500': '#ef4444',
+    'yellow-500': '#facc15',
+};
+
 const Task = ({ task, onComplete, onEdit, onDelete, color }) => {
-    const checkedBgClass = `checked:bg-${color}`;
+    const inputBgColor = tailwindColors[color] || 'transparent';
     return (
         <div className="flex items-center justify-between p-3 mb-2 bg-zinc-700 rounded shadow">
             <div className="relative flex rounded-full cursor-pointer">
-                <input
-                    type="checkbox"
+                <input type="checkbox"
                     checked={task.isCompeleted}
                     // onChange={() => onComplete(task.id)}
-                    className={`before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-none ${checkedBgClass}`}
-                />
-                <span
-                    className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
+                    style={task.isCompeleted ? { backgroundColor: inputBgColor } : {}}
+                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-none" />
+                <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                    <svg xmlns="http://www.w3.org/2000/svg"
                         className="h-3 w-3"
                         viewBox="0 0 20 20"
                         fill="currentColor"
                         stroke="currentColor"
-                        strokeWidth="1"
-                    >
-                        <path
-                            fillRule="evenodd"
+                        strokeWidth="1">
+                        <path fillRule="evenodd"
                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clipRule="evenodd"
-                        />
+                            clipRule="evenodd" />
                     </svg>
                 </span>
             </div>
@@ -39,20 +38,17 @@ const Task = ({ task, onComplete, onEdit, onDelete, color }) => {
                 </span>
             </div>
             <div className="flex items-center">
-                <FontAwesomeIcon
-                    icon={task.isImportant ? solidStar : regularStar}
+                <FontAwesomeIcon icon={task.isImportant ? solidStar : regularStar}
                     className={`text-${color} cursor-pointer mr-2`}
-                // onClick={() => onEdit(task.id)}
+                // onClick={() => onEdit(task.id)} 
                 />
-                <FontAwesomeIcon
-                    icon={faEdit}
+                <FontAwesomeIcon icon={faEdit}
                     className={`text-${color} cursor-pointer mr-2`}
-                // onClick={() => onEdit(task.id)}
+                // onClick={() => onEdit(task.id)} 
                 />
-                <FontAwesomeIcon
-                    icon={faTrashAlt}
+                <FontAwesomeIcon icon={faTrashAlt}
                     className={`text-${color} cursor-pointer mr-2`}
-                // onClick={() => onDelete(task.id)}
+                // onClick={() => onDelete(task.id)} 
                 />
             </div>
         </div>
--- src/Pages/Tasks.js
+++ src/Pages/Tasks.js
@@ -5,10 +5,11 @@ import ButtonAdd from "../Components/ButtonAdd";
 import PropTypes from "prop-types";
 
 const Tasks = ({ color }) => {
+    const url = "http://localhost:5000/api/v1/tasks";
     return (
         <div className="w-3/4 bg-zinc-800 m-4 rounded-xl p-4 overflow-hidden flex flex-col">
             <Title text="Tasks" icon={faHouse} color={'text-' + color} />
-            <TaskList color={color}/>
+            <TaskList color={color} url={url} />
             <ButtonAdd border_color="border-pink-300" text_color={'text-' + color} hover_color="hover:bg-pink-300" />
         </div>
     );
