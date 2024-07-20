c67ffedcbc141e0a4f1ef57447c17fb3ffc412f6
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-07-20 15:45:15 +0700

    #32 feat: add new task (#33)

--- src/Pages/Important.js
+++ src/Pages/Important.js
@@ -1,22 +1,25 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Title from "../Components/Title";
 import TaskList from "../Components/TaskList";
-import ButtonAdd from "../Components/ButtonAdd";
-import PropTypes from "prop-types";
+import useApi from "../Hooks/UseApi";
 
-const Important = ({ color }) => {
+const Important = () => {
     const url = 'http://localhost:5000/api/v1/tasks/important';
+    const { data: tasks, refetch } = useApi(url);
+    const handleRefetch = () => {
+        refetch();
+    };
     return (
         <div className="w-3/4 bg-zinc-800 m-4 rounded-xl p-4 overflow-hidden flex flex-col">
-            <Title text="Important" icon={faStar} color={'text-' + color} />
-            <TaskList color={color} url={url} hover_color={'hover:bg-yellow-500'}/>
-            <ButtonAdd border_color="border-yellow-200" text_color="text-yellow-500" hover_color="hover:bg-yellow-200" />
+            <Title text="Important" icon={faStar} color="text-yellow-500" />
+            <TaskList 
+                color="yellow-500"
+                url={url} 
+                hover_color={'hover:bg-yellow-500'}
+                data={tasks} 
+                onRefetch={handleRefetch} />
         </div>
     );
-};
-
-Important.propTypes = {
-    color: PropTypes.string.isRequired
 };
 
 export default Important;
--- src/Pages/Tasks.js
+++ src/Pages/Tasks.js
@@ -1,22 +1,29 @@
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import Title from "../Components/Title";
 import TaskList from "../Components/TaskList";
-import ButtonAdd from "../Components/ButtonAdd";
-import PropTypes from "prop-types";
+import InputTask from "../Components/InputTask";
+import useApi from "../Hooks/UseApi";
 
-const Tasks = ({ color }) => {
+const Tasks = () => {
     const url = "http://localhost:5000/api/v1/tasks";
+    const { data: tasks, refetch } = useApi(url);
+    const handleRefetch = () => {
+        refetch();
+    };
+
     return (
         <div className="w-3/4 bg-zinc-800 m-4 rounded-xl p-4 overflow-hidden flex flex-col">
-            <Title text="Tasks" icon={faHouse} color={'text-' + color} />
-            <TaskList color={color} url={url} hover_color={'hover:bg-red-500'}/>
-            <ButtonAdd border_color="border-pink-300" text_color={'text-' + color} hover_color="hover:bg-pink-300" />
+            <Title text="Tasks" icon={faHouse} color="text-red-500" />
+            <TaskList
+                color="red-500"
+                url={url}
+                hover_color="hover:bg-red-500"
+                data={tasks}
+                onRefetch={handleRefetch}
+            />
+            <InputTask onRefetch={handleRefetch} />
         </div>
     );
-};
-
-Tasks.propTypes = {
-    color: PropTypes.string.isRequired
 };
 
 export default Tasks;
