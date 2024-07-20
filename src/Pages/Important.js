e3b0d33c4f3465d712dd0dd8e24a2800dc05da3e
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-20 15:43:47 +0700

    feat: remove add input item

--- src/Pages/Important.js
+++ src/Pages/Important.js
@@ -1,11 +1,9 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Title from "../Components/Title";
 import TaskList from "../Components/TaskList";
-import PropTypes from "prop-types";
 import useApi from "../Hooks/UseApi";
-import InputTask from "../Components/InputTask";
 
-const Important = ({ color }) => {
+const Important = () => {
     const url = 'http://localhost:5000/api/v1/tasks/important';
     const { data: tasks, refetch } = useApi(url);
     const handleRefetch = () => {
@@ -13,20 +11,15 @@ const Important = ({ color }) => {
     };
     return (
         <div className="w-3/4 bg-zinc-800 m-4 rounded-xl p-4 overflow-hidden flex flex-col">
-            <Title text="Important" icon={faStar} color={'text-' + color} />
+            <Title text="Important" icon={faStar} color="text-yellow-500" />
             <TaskList 
-                color={color} 
+                color="yellow-500"
                 url={url} 
                 hover_color={'hover:bg-yellow-500'}
                 data={tasks} 
                 onRefetch={handleRefetch} />
-            <InputTask onRefetch={handleRefetch} />
         </div>
     );
-};
-
-Important.propTypes = {
-    color: PropTypes.string.isRequired
 };
 
 export default Important;
