4ce6d56d2d0c277206b6b7ac1b3f2b186d1f732f
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-07-20 10:50:10 +0700

    #30 feat: edit task (#31)

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,6 +3,7 @@ import { faStar as solidStar, faEdit, faTrashAlt } from '@fortawesome/free-solid
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
 
 const tailwindColors = {
     'pink-300': '#f472b6',
@@ -12,6 +13,9 @@ const tailwindColors = {
 
 const Task = ({ task, color, onRefetch, hover_color }) => {
     const inputBgColor = tailwindColors[color] || 'transparent';
+    const [isEdit, setIsEdit] = useState(false);
+    const [taskName, setTaskName] = useState(task.name);
+    const inputRef = useRef(null);
     const handleComplete = async (id) => {
         await axios({
             method: 'PATCH',
@@ -33,14 +37,38 @@ const Task = ({ task, color, onRefetch, hover_color }) => {
         });
         onRefetch();
     }
+    const handleEdit = async (id) => {
+        await axios({
+            method: 'PATCH',
+            url: 'http://localhost:5000/api/v1/tasks/' + id + '/update',
+            data: {
+                name: taskName,
+            },
+            headers: { "Content-Type": "multipart/form-data" },
+        });
+        setIsEdit(false);
+        onRefetch();
+    }
+    const handleKeyDown = (event, id) => {
+        if (event.key === 'Enter') {
+            handleEdit(id);
+        }
+    }
+    useEffect(() => {
+        if (isEdit && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isEdit]);
     return (
         <div className="flex items-center justify-between p-3 mb-2 bg-zinc-700 rounded shadow">
             <div className="relative flex rounded-full cursor-pointer">
-                <input type="checkbox"
+                <input
+                    type="checkbox"
                     checked={task.isCompeleted}
                     onChange={() => handleComplete(task.id)}
                     style={task.isCompeleted ? { backgroundColor: inputBgColor } : {}}
-                    className={`before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-none hover:border-none ${hover_color}`} />
+                    className={`before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-white transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:opacity-0 before:transition-opacity checked:border-none hover:border-none ${hover_color}`}
+                />
                 <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100 peer-hover:opacity-100">
                     <svg xmlns="http://www.w3.org/2000/svg"
                         className="h-3 w-3"
@@ -55,21 +83,39 @@ const Task = ({ task, color, onRefetch, hover_color }) => {
                 </span>
             </div>
             <div className="flex-1 px-4">
-                <span className={`${task.isCompeleted ? 'line-through text-zinc-800' : 'text-white'} text-left text-sm`}>
-                    {task.name}
-                </span>
+                {isEdit ? (
+                    <input
+                        type="text"
+                        className="w-full bg-transparent text-white border-none focus:outline-none"
+                        value={taskName}
+                        onChange={(e) => setTaskName(e.target.value)}
+                        onBlur={() => handleEdit(task.id)}
+                        onKeyDown={(e) => handleKeyDown(e, task.id)}
+                        ref={inputRef}
+                    />
+                ) : (
+                    <span className={`${task.isCompeleted ? 'line-through text-zinc-800' : 'text-white'} text-left text-sm cursor-pointer`}>
+                        {task.name}
+                    </span>
+                )}
             </div>
             <div className="flex items-center">
-                <FontAwesomeIcon icon={task.isImportant ? solidStar : regularStar}
+                <FontAwesomeIcon
+                    icon={task.isImportant ? solidStar : regularStar}
                     className={`text-${color} cursor-pointer mr-2`}
                     title='Important'
-                    onClick={() => handleImportant(task.id)} 
+                    onClick={() => handleImportant(task.id)}
                 />
-                <FontAwesomeIcon icon={faEdit}
-                    className={`text-${color} cursor-pointer mr-2`}
-                // onClick={() => onEdit(task.id)} 
-                />
-                <FontAwesomeIcon icon={faTrashAlt}
+                {!task.isCompeleted && (
+                    <FontAwesomeIcon
+                        icon={faEdit}
+                        className={`text-${color} cursor-pointer mr-2`}
+                        title='Edit'
+                        onClick={() => setIsEdit(true)}
+                    />
+                )}
+                <FontAwesomeIcon
+                    icon={faTrashAlt}
                     className={`text-${color} cursor-pointer mr-2`}
                     title='Delete'
                     onClick={() => handleDelete(task.id)}
@@ -83,7 +129,7 @@ Task.propTypes = {
     task: PropTypes.object.isRequired,
     color: PropTypes.string.isRequired,
     onRefetch: PropTypes.func,
-    hover_color: PropTypes.string
+    hover_color: PropTypes.string,
 };
 
 export default Task;
