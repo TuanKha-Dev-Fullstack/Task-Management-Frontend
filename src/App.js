704652869992206577515b55d7b6c045d9c887a5
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-07-22 11:17:49 +0700

    #42 fix: re-fetch task when deleting category (#43)

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Tasks from "./Pages/Tasks";
 import Important from "./Pages/Important";
 import Categories from "./Pages/Categories";
 import useApi from "./Hooks/UseApi";
+import { useState } from "react";
 const App = () => {
   const url = 'http://localhost:5000/api/v1/categories';
   const { data: categories, refetch } = useApi(url);
+  const [refetchTasks, setRefetchTasks] = useState(false);
+  const handleRefetchTasks = () => {
+    setRefetchTasks(!refetchTasks);
+  }
   return (
     <div className="flex h-screen bg-black gap-5">
       <BrowserRouter>
-        <SideBar categories={categories} refetch={refetch} />
+        <SideBar categories={categories} refetch={refetch} onChange={handleRefetchTasks} />
         <Routes>
-          <Route path="/" element={<Tasks />} />
+          <Route path="/" element={<Tasks refetchTasks={refetchTasks} />} />
           <Route path="/important" element={<Important />} />
           {categories?.map((category) => (
             <Route key={category.id}
